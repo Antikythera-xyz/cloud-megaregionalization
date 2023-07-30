f000b75fd572513f19ff04f28219b33ad77e2965
Sarah Scott <sarahoscott@gmail.com>
2023-07-30 10:41:51 -0700

    antikythera basepath

---
+++ _app/immutable/chunks/paths.3ac2867f.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1cew0k0)==null?void 0:s.base)??"/cloud-megaregionalization";var e;const t=((e=globalThis.__sveltekit_1cew0k0)==null?void 0:e.assets)??a;export{t as a,a as b};
